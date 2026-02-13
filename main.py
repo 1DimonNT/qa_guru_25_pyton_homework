49170c4373a269ea8f62958ac6856d3421674a71
Dmitrii Ivantsov <1DimonNT@gmail.com>
2026-02-13 14:42:18 +0500

    add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# Стандартный шаблон "Hello, World" в PyCharm
+def print_hi(name):
+    # Используйте брейкпоинт в строке кода ниже для отладки
+    print(f'Hi, {name}')  # Нажмите Ctrl+F8 (или Cmd+F8 на Mac)
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
